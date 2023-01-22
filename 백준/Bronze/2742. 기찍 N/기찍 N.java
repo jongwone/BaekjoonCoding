bfe89dd8fb30db9c99d6b75ae56cd07e8d61d8bd
jongwone <123169841+jongwone@users.noreply.github.com>
2023-01-22 15:11:26 +0900

    [Bronze IV] Title: 기찍 N, Time: 1076 ms, Memory: 31736 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = n; i > 0; i--)
+            System.out.println(i);
+    }
+}
