c68967d407ed1c22d1d1965117c690315bb50f0f
jongwone <123169841+jongwone@users.noreply.github.com>
2023-01-25 02:01:20 +0900

    [Bronze II] Title: 16진수, Time: 200 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        String n = scan.next();
+        System.out.println(Integer.parseInt(n, 16));
+    }
+}
